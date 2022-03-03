0fde15edd48ff8d6afc08f5fc70cfad649b7cf4e
LZ <lizong20001204@gmail.com>
2022-03-03 15:53:01 +0800

    feat: add binary tree training
    力扣 124题 后序遍历

--- src/binaryTree/二叉树中的最大路径和.java
+++ src/binaryTree/二叉树中的最大路径和.java
@@ -1,22 +1,40 @@
 package binaryTree;
 
 /**
- * Definition for a binary tree node.
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/
+ * 力扣 124 二叉树中的最大路径和
+ * 本质上就是一个后序遍历
  */
 class 二叉树中的最大路径和 {
     public static void main(String[] args) {
-
+        System.out.println(maxPathSum(buildTree()));
     }
 
-    int maxPathSum = Integer.MIN_VALUE;
+    static int maxPathSum = Integer.MIN_VALUE;
 
-    public int maxPathSum(TreeNode root) {
+    public static int getMaxPathSum(TreeNode root) {
         if (root == null) return 0;
-
-        return 1;
+        int left = Math.max(0, getMaxPathSum(root.left));
+        int right = Math.max(0, getMaxPathSum(root.right));
+        // 后序遍历代码位置
+        maxPathSum = Math.max(maxPathSum, left + right + root.val);
+        return Math.max(left, right) + root.val;
     }
 
-    class TreeNode {
+    public static int maxPathSum(TreeNode root) {
+        getMaxPathSum(root);
+        return maxPathSum;
+    }
+
+    public static TreeNode buildTree() {
+        TreeNode right1_left2 = new TreeNode(15);
+        TreeNode right1_right2 = new TreeNode(7);
+        TreeNode left1 = new TreeNode(9);
+        TreeNode right1 = new TreeNode(20, right1_left2, right1_right2);
+        return new TreeNode(-10, left1, right1);
+    }
+
+    static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
