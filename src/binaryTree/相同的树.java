6b2ac806e8397854d2317f9b58ef83ff9dc19263
LZ <lizong20001204@gmail.com>
2022-03-05 09:39:41 +0800

    feat: add binary tree training
    力扣 100题 前序遍历

---
+++ src/binaryTree/相同的树.java
@@ -0,0 +1,55 @@
+package binaryTree;
+
+/**
+ * https://leetcode-cn.com/problems/same-tree/
+ * 力扣 100 相同的树
+ */
+public class 相同的树 {
+    public static void main(String[] args) {
+        System.out.println(isSameTree(buildTree(), buildTree2()));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSame(p, q);
+    }
+
+    public static boolean isSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSame(p.left, q.left) && isSame(p.right, q.right);
+    }
+
+    public static TreeNode buildTree() {
+        TreeNode right1_left2 = new TreeNode(15);
+        TreeNode right1_right2 = new TreeNode(7, new TreeNode(1), null);
+        TreeNode left1 = new TreeNode(9);
+        TreeNode right1 = new TreeNode(20, right1_left2, right1_right2);
+        return new TreeNode(3, left1, right1);
+    }
+
+    public static TreeNode buildTree2() {
+        TreeNode right1_left2 = new TreeNode(15);
+        TreeNode right1_right2 = new TreeNode(7, new TreeNode(1), null);
+        TreeNode left1 = new TreeNode(9);
+        TreeNode right1 = new TreeNode(20, right1_left2, right1_right2);
+        return new TreeNode(3, left1, right1);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
