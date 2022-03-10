f17b15dcfc262769916e953c082f1e0bf023203e
LZ <lizong20001204@gmail.com>
2022-03-10 14:23:48 +0800

    feat: add hash training
    力扣 217题 存在重复元素

---
+++ src/hash/存在重复元素.java
@@ -0,0 +1,24 @@
+package hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://leetcode-cn.com/problems/contains-duplicate/
+ * 力扣 217 存在重复元素
+ */
+public class 存在重复元素 {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[] {1, 2, 3, 4}));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
