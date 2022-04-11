3633eadefe611c1bd1ada79707d91f81d80d4caa
LZ <lizong20001204@gmail.com>
2022-04-11 16:59:26 +0800

    feat: add array training 力扣 509 斐波那契数 更优解

--- src/斐波那契数.java
+++ src/斐波那契数.java
@@ -4,7 +4,8 @@
  */
 public class 斐波那契数 {
     public static void main(String[] args) {
-        System.out.println(fib(5));
+        System.out.println(fib(7));
+        System.out.println(fib1(7));
     }
 
     public static int fib(int n) {
@@ -17,5 +18,19 @@ public class 斐波那契数 {
             arr[i] = arr[i - 1] + arr[i - 2];
         }
         return arr[n - 1];
+    }
+
+    // 更优解，空间复杂度O(1)
+    public static int fib1(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int var1 = 1, var2 = 2;
+        int box = 0;
+        for (int i = 3; i < n; i++) {
+            box = var1 + var2;
+            var1 = var2;
+            var2 = box;
+        }
+        return box;
     }
 }
